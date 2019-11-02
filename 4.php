0e8350710cca23419863bd9a1d8385e8f5c31c43
khilmi aminudin <khilmiaminudin715@gmail.com>
2019-11-02 14:52:33 +0700

    mengerjakan soal nomor 4

---
+++ 4.php
@@ -0,0 +1,33 @@
+<?php
+
+    function barang($barang,$qty){
+     
+        
+        switch ($barang) {
+            case 'A':
+                $h = 5000;
+            break;
+            case 'B':
+                $h = 3500;
+            break;
+            case 'C':
+                $h = 2500;
+            break;
+            default:
+                echo "Barang yang anda masukkan salah";    
+            break;
+        }
+
+
+        if ($h == 5000 && $qty > 10) {
+            $disc = $qty * $h - 500;
+        }elseif ($h == 3500 && $qty > 5 ) {
+            $disc = $qty * $h * 0.5;
+        }else {
+            echo 'barang ini belum ada diskon';
+        }
+
+        echo "Kualitas barang ".$barang." jumlah Barang ".$qty." maka anda mendapatkan diskon sebesar ".$disc;
+    }
+
+    barang(A,11);
