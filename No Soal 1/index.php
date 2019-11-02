4e7c4d89ad9a6a10a5390281f9b6225f95b3f838
khilmi aminudin <khilmiaminudin715@gmail.com>
2019-11-02 13:32:27 +0700

    memasukan php dan me logic nya

--- No Soal 1/index.php
+++ No Soal 1/index.php
@@ -1,3 +1,15 @@
+<?php
+    // membuat array
+    $arr    = [1,2,3,4,5];
+
+    $hasil  = array_sum($arr);
+
+    $arr_1 = $hasil-$arr[0];
+    $arr_2 = $hasil-$arr[1];
+    $arr_3 = $hasil-$arr[2];
+    $arr_4 = $hasil-$arr[3];
+    $arr_5 = $hasil-$arr[4];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +19,14 @@
     <title>Document</title>
 </head>
 <body>
+
+    <ol>
+        <li>index Array <? $arr[0] ?>:<? $arr_1 ?></li>
+        <li>index Array <? $arr[1] ?>:<? $arr_2 ?></li>
+        <li>index Array <? $arr[2] ?>:<? $arr_3 ?></li>
+        <li>index Array <? $arr[3] ?>:<? $arr_4 ?></li>
+        <li>index Array <? $arr[4] ?>:<? $arr_5 ?></li>
+    </ol>
     
 </body>
 </html>
