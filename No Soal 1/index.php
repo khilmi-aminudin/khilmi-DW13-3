f953872307024ccf3a95495c23fbf8ae2b8d14be
khilmi aminudin <khilmiaminudin715@gmail.com>
2019-11-02 13:59:18 +0700

    memodifikasi tabel

--- No Soal 1/index.php
+++ No Soal 1/index.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-
+<h1>Soal No 1</h1>
 <table class="table">
   <thead class="thead-dark">
     <tr>
@@ -52,6 +52,20 @@
       <td><?= $arr_5 ?></td>
     </tr>
     </tbody>
+    <tfoot>
+        <tr>
+            <th scope="row">Jumlah</th>
+            <td><?= $hasil ?></td>
+        </tr>
+        <tr>
+            <th scope="row">Nilai Tertinggi</th>
+            <td><?= $max ?></td>
+        </tr>
+        <tr>
+            <th scope="row">Nilai Terendah</th>
+            <td><?= $min ?></td>
+        </tr>
+    </tfoot>
 </table>
     
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
