8edafad5e8e9686f6248df4a6ab7c22c7cf5c889
khilmi aminudin <khilmiaminudin715@gmail.com>
2019-11-02 17:12:48 +0700

    edit file 2

--- 2.php
+++ 2.php
@@ -1,39 +1,64 @@
 <?php
-
+    $bayar = $_POST['byr'];
+    $uang = $_POST['uang'];
     function kembalian($bayar,$uang){
         $kembali=$uang-$bayar;
 
         return $kembali;
     }
 
-    $kembalian = kembalian(12400,15000);
-    echo $kembalian;
-
-    if($kembalian >= 50000){
-
-    }
-    elseif($kembalian){
-
-    }
+ 
+    $kembalian = kembalian($bayar,$uang);
 ?>
 
 
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Document</title>
+        <title>Document</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
-<body>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+        <form method="POST">
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
+            <table>
+                <tr>
+                    <td>Uang </td>
+                    <td>
+                        <input type="text" placeholder="masukkan jumlah uang" name="uang">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Bayar</td>
+                    <td>
+                    <input type="text" placeholder="Jumalah bayar" name="byr">
+                    </td>
+                </tr>
+                <tr colspan="2">
+                    <td>
+                    <input type="submit">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Kembalian</td>
+                    <td><?= $kembalian ?></td>
+                </tr>
+            </table>
+
+
+
+
+        </form>
+
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
 </html>
